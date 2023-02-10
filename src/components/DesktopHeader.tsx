f22317c4ecc4b12b05afbc476cb20713405cb8ca
Mario Kovac <mkovac25122512@gmail.com>
2023-02-10 20:02:09 +0100

    Add search button to desktop header input element

--- src/components/DesktopHeader.tsx
+++ src/components/DesktopHeader.tsx
@@ -12,17 +12,42 @@ const DesktopHeader = () => {
     }
   };
 
+  const updateSearchQueryBtn = () => {
+    const inputElement = document.querySelector(
+      ".desktop-header-search__input"
+    );
+    const currentQuery = inputElement as HTMLInputElement;
+
+    if (
+      currentQuery.value !== "" &&
+      currentQuery.value !== null &&
+      currentQuery.value !== undefined
+    ) {
+      localStorage.setItem("searchQuery", currentQuery.value);
+      window.location.href = "/search";
+      currentQuery.value = "";
+    }
+  };
+
   return (
     <div className="desktop-header-container">
       <h1 className="desktop-header-title">
         <span className="desktop-header-title-red">My</span>News
       </h1>
-      <input
-        className="search-button"
-        placeholder="Search"
-        type="text"
-        onKeyDown={updateSearchQuery}
-      ></input>
+      <div className="desktop-header-search__submit">
+        <input
+          className="desktop-header-search__input"
+          placeholder="Search"
+          type="text"
+          onKeyDown={updateSearchQuery}
+        ></input>
+        <button
+          className="desktop-header-search__button"
+          onClick={updateSearchQueryBtn}
+        >
+          SEARCH
+        </button>
+      </div>
     </div>
   );
 };
